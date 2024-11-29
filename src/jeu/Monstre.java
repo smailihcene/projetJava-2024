a5285f86cf2588a3bb84350562ead8aa974c2765
smailihcene <cheimasmail02@gmail.com>
2024-11-29 14:53:42 +0100

    test 2 de git

---
+++ src/jeu/Monstre.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jeu;
+
+/**
+ *
+ * @author cheim
+ */
+public class Monstre {
+
+    private String monstre;
+}
